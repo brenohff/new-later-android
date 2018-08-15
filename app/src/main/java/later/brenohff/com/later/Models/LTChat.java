a0d4a36e5a25a0db9df1ee87cdd4e018247e6bbf
Breno Henrique <brenohff@gmail.com>
2018-08-15 00:02:28 -0300

    Atualizacao do gadle; Correção no setUser do LTChat

--- app/src/main/java/later/brenohff/com/later/Models/LTChat.java
+++ app/src/main/java/later/brenohff/com/later/Models/LTChat.java
@@ -64,7 +64,7 @@ public class LTChat implements Serializable {
         return users;
     }
 
-    public void setUser(LTUser user) {
+    public void setUser(LTUser users) {
         this.users = users;
     }
 
